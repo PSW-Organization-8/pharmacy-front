bb1b4e3e25e842fc12631e84d5693a9cb52a4a11
MatijaUskokovic <uskokovic.ra76.2018@uns.ac.rs>
2021-11-16 18:29:22 +0100

    feat: implemented frontend for creating news and actions

--- pharmacy-app/src/app/app.module.ts
+++ pharmacy-app/src/app/app.module.ts
@@ -1,15 +1,41 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+
+import { CreateNewsComponent } from './create-news/create-news.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'createActionsAndNews', component: CreateNewsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CreateNewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    CommonModule,
+    ToastrModule.forRoot(),
+    RouterModule.forRoot(routes),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
