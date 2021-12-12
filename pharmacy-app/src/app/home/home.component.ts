a42048662500a7c29ca3f37b2d2773db99c1a41c
Vladimir <74308447+vladimirr9@users.noreply.github.com>
2021-12-12 16:02:08 +0100

    feat: env var (#6)
    * feat: added back URL as env var
    
    * feat: test docker
    
    * feat: test docker 2
    
    * feat: test docker 3

--- pharmacy-app/src/app/home/home.component.ts
+++ pharmacy-app/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { pharmacyServerPort } from '../app.consts';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get(`http://localhost:8080/pharmacy`).subscribe((data) => {
+      console.log(data);
+
+    })
   }
 
 }
