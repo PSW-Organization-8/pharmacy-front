f0a0500a71ffde8ed843c75a611200487b4821ce
RA78-2018 <74359093+RA78-2018@users.noreply.github.com>
2022-01-25 16:58:39 +0100

    fix: validation for tender offer

--- pharmacy-app/src/app/create-offer/create-offer.component.ts
+++ pharmacy-app/src/app/create-offer/create-offer.component.ts
@@ -16,7 +16,7 @@ export class CreateOfferComponent implements OnInit {
   medicationList: Array<OfferMedication>;
   pharmacies: any;
   selectedPharmacy: any;
-  availableMedication: any;
+  availableMedication:any;
 
   selectedMedication: any;
   enteredQuantity: any;
@@ -33,6 +33,8 @@ export class CreateOfferComponent implements OnInit {
       this.isModalActive = false;
       this.selectedPharmacy = 0;
       this.isListEmpty = true;
+      this.enteredPrice = 0;
+      this.enteredQuantity = 0;
      }
 
 
@@ -73,8 +75,6 @@ export class CreateOfferComponent implements OnInit {
     this.offerService.getMedicationForPharmacy(this.selectedPharmacy).subscribe(
       (data)=>{
         this.availableMedication = data
-        this.isModalActive = true;
-        console.log(this.isModalActive);
       },
       (error)=>{
         this.toastr.error("There is an error")
@@ -83,13 +83,27 @@ export class CreateOfferComponent implements OnInit {
   }
 
   addMedication(): void{
-    let medication = new OfferMedication(this.selectedMedication.id, this.selectedMedication.name, this.enteredQuantity, this.enteredPrice);
+    let medication = new OfferMedication(this.selectedMedication.medication.id, this.selectedMedication.medication.name, this.enteredQuantity, this.enteredPrice);
+    if(this.selectedMedication.quantity > this.enteredQuantity){
     this.medicationList.push(medication);
     this.isListEmpty = false;
+    this.toggleModal();
+    }
+    else{
+      this.toastr.error("Medication quantity not available.")
+    }
   }
 
-  changeOffer(id: any){
-    this.selectedMedication = id;
+  medicationExists(id: any){
+    if(this.medicationList.find(x=> x.MedicationId == id) != undefined){
+      return false;
+    }
+    return true;
+  }
+
+  changeOffer(medication: any){
+    this.selectedMedication = medication;
+    this.isModalActive = true;
   }
 
   toggleModal() {
